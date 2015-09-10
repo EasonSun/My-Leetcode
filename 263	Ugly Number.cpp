c61f062d4c84c8ea57eb382b2230b77d1d4e9973
EasonSun <ysun73@illinois.edu>
2015-09-10 00:12:53 -0500

    Create 263	Ugly Number.cpp

---
+++ 263	Ugly Number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isUgly(int num) {
+        if(num == 0) {return 0;}
+        while(num%2 == 0) {
+            num = num/2;
+            if(num==1) return 1;
+        }
+        
+        while(num%3 == 0) {
+            num = num/3;
+            if(num==1) return 1;
+        }
+        
+        while(num%5 == 0) {
+            num = num/5;
+            if(num==1) return 1;
+        }
+        
+        if(num == 1) return 1;
+        return 0;
+    }
+};
