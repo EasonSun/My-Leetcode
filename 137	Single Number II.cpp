6b91a0501b6868a27e18cd2f6663a057382124a8
EasonSun <ysun73@illinois.edu>
2015-09-22 13:51:04 -0500

    Update 137	Single Number II.cpp

--- 137	Single Number II.cpp
+++ 137	Single Number II.cpp
@@ -2,22 +2,27 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        //we need to implement a tree-time counter(base 3) that if a bit appears three time ,it will be zero.
+        //Here a generic way is to count, and actually count the bits
+        //Input a int, which is 32 bits, count the time these bits appear
+        //We need to implement a tree-time counter(base 3) that if a bit appears three time ,it will be zero.
+        //The counter runs on all bits of an integer at once, by bit operation 
         //#curent  income  ouput
         //# ab      c/c       ab/ab
         //# 00      1/0       01/00
         //# 01      1/0       10/01
         //# 10      1/0       00/10
-        // a=~abc+a~b~c;
-        // b=~a~bc+~ab~c;
+        // a = ~abc + a~b~c; intepret the only cases when a is 1
+        // b = ~a~bc + ~ab~c; intepret the only cases when b is 1
         int a=0;
         int b=0;
         for(int c:nums){
-            int ta=(~a&b&c)|(a&~b&~c);
-            b=(~a&~b&c)|(~a&b&~c);
-            a=ta;
+            int nexta=(~a&b&c)|(a&~b&~c);
+            int nextb=(~a&~b&c)|(~a&b&~c);
+            a = nexta;
+            b = nextb;
         }
-        //we need find the number that is 01,10 => 1, 00 => 0.
-        return a|b;
+        //Here we can should output b, because the target number appers only once, rendering the state stops at ab=01
+        //So we can output any single number, given the time it appers
+        return b;
     }
 };
