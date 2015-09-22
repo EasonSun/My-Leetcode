d225d5790c89e5e08d1f7360a4a0f722cbe5aeeb
EasonSun <ysun73@illinois.edu>
2015-09-22 10:40:24 -0500

    Update 136	Single Number.cpp

--- 136	Single Number.cpp
+++ 136	Single Number.cpp
@@ -4,8 +4,8 @@ public:
         int n = nums.size();
         int ans = 0;
         for(int i=0;i<n;i++){
-            //It is a xor operator. We use it here because of following properties.
-            //1) x^x = 0. 2) 0^x = x.
+            //bitwise xor
+            //actually not a running result, but rather xor everything so order does not matter
             ans ^= nums[i];
         }
         return ans;
